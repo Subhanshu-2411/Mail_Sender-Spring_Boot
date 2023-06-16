1f273eefcc772e9b1770a9a7cb4492a966f12be8
Subhanshu-2411 <subhashu20135@iiitd.ac.in>
2023-06-17 03:36:00 +0530

    Initial Commit

---
+++ src/test/java/com/example/mail_senderspring_boot/MailSenderSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mail_senderspring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
